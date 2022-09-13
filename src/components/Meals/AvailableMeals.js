7d471d513271fb3721a9dbb4e14c14b1a3fa2518
Stefan Zugun <stefan.zugun@bath.edu>
2022-09-13 16:18:13 +0300

    Added MealItems

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "../UI/Card";
+
+import classes from "./AvailableMeals.module.css";
+import MealItem from "./MealItem";
+
+const AvailableMeals = (props) => {
+  const mealsList = props.meals.map((item) => (
+    <MealItem
+      name={item.name}
+      description={item.description}
+      price={item.price}
+    />
+  ));
+
+  return (
+    <div className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </div>
+  );
+};
+
+export default AvailableMeals;
