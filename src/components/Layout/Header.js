eac68c9da877f8fc2e7bdd03a8c3d0397433b9d8
Stefan Zugun <stefan.zugun@bath.edu>
2022-09-13 15:22:19 +0300

    HeaderCartButton added

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,20 @@
 import React from "react";
 
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton text="Your Cart" />
       </header>
       <div className={classes["main-image"]}>
-        <img src="https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg" />
+        <img
+          src="https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg"
+          alt=""
+        />
       </div>
     </>
   );
