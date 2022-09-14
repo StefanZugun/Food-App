f706f609e850d82c2fce4c856e7f439273c063dd
Stefan Zugun <stefan.zugun@bath.edu>
2022-09-14 17:28:12 +0300

    Cart shows different message if no items are in it

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
 
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+      {["Sushi"].map((item) => (
         <div key={item.id}>
           <li>{item.name}</li>
         </div>
       ))}
     </ul>
   );
-
+  console.log(cartCtx);
   return (
     <Modal onCloseCart={props.onCloseCart}>
       {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>35.62</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
-          Close
-        </button>
-        <button className={classes.button}>Order</button>
-      </div>
+      {cartCtx.totalAmount !== 0 && (
+        <div>
+          <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>{cartCtx.totalAmount}</span>
+          </div>
+          <div className={classes.actions}>
+            <button
+              className={classes["button--alt"]}
+              onClick={props.onCloseCart}
+            >
+              Close
+            </button>
+            <button className={classes.button}>Order</button>
+          </div>
+        </div>
+      )}
+      {cartCtx.totalAmount === 0 && (
+        <div className={classes.total}>
+          <h5>
+            You Don't have anything ordered! Please add items to eat today!
+          </h5>
+          <div className={classes.actions}>
+            <button
+              className={classes["button--alt"]}
+              onClick={props.onCloseCart}
+            >
+              Add Items
+            </button>
+          </div>
+        </div>
+      )}
     </Modal>
   );
 };
